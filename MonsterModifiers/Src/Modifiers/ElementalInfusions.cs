aece91554e45252c55d5239c4fdc9c733581ec0e
jneb802 <73610029+jneb802@users.noreply.github.com>
2025-01-05 21:04:24 -0800

    Nerf ElementalInfusions.cs

--- MonsterModifiers/Src/Modifiers/ElementalInfusions.cs
+++ MonsterModifiers/Src/Modifiers/ElementalInfusions.cs
@@ -35,25 +35,25 @@ public class ElementalInfusions
 
             if (modiferComponent.Modifiers.Contains(MonsterModifierTypes.PoisonInfused))
             {
-                hit.m_damage.m_poison += hit.GetTotalDamage();
+                hit.m_damage.m_poison += (hit.GetTotalDamage() * 0.5f);
                 // Debug.Log("Hit has additional poison damage added. Amount is: " + hit.m_damage.m_poison);
             }
             
             if (modiferComponent.Modifiers.Contains(MonsterModifierTypes.FireInfused))
             {
-                hit.m_damage.m_fire += hit.GetTotalDamage();
+                hit.m_damage.m_fire += (hit.GetTotalDamage() * 0.5f);
                 // Debug.Log("Hit has additional fire damage added. Amount is: " + hit.m_damage.m_fire);
             }
             
             if (modiferComponent.Modifiers.Contains(MonsterModifierTypes.LightningInfused))
             {
-                hit.m_damage.m_lightning += hit.GetTotalDamage();
+                hit.m_damage.m_lightning += (hit.GetTotalDamage() * 0.5f);
                 // Debug.Log("Hit has additional lightning damage added. Amount is: " + hit.m_damage.m_lightning);
             }
             
             if (modiferComponent.Modifiers.Contains(MonsterModifierTypes.FrostInfused))
             {
-                hit.m_damage.m_frost += hit.GetTotalDamage();
+                hit.m_damage.m_frost += (hit.GetTotalDamage() * 0.5f);
                 // Debug.Log("Hit has additional frost damage added. Amount is: " + hit.m_damage.m_frost);
             }
         }
