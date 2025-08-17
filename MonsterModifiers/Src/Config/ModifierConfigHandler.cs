92cf38ee071280a013862922f6a06c1eefc5cd25
senft-research <JordanSenft@proton.me>
2025-08-17 18:53:33 +0100

    - Clean up of ModifierConfigHandler.cs logic, along with exceptions thrown in invalid cases
    - MonsterModifiers.csproj updated with location of ModifierConfigHandler.cs in compile includes.

--- MonsterModifiers/Src/Config/ModifierConfigHandler.cs
+++ MonsterModifiers/Src/Config/ModifierConfigHandler.cs
@@ -2,16 +2,17 @@ using System;
 using System.Collections.Generic;
 using BepInEx.Configuration;
 using Jotunn.Extensions;
+using UnityEngine;
 
 namespace MonsterModifiers.Config;
 
 /// <summary>
-/// Handler for the values determined by the Bepinex Config for this mod. 
+/// Handler for the values determined by the Bepinex Config for this mod.
 /// </summary>
 public class ModifierConfigHandler
 {
-    ConfigFile _configFile;
-    Dictionary<MonsterModifierTypes, ConfigEntry<int>> configModifierWeights = new();
+    ConfigFile? _configFile;
+    readonly Dictionary<MonsterModifierTypes, ConfigEntry<int>> _configModifierWeights = new();
 
     static ModifierConfigHandler? _instance;
 
@@ -20,7 +21,8 @@ public class ModifierConfigHandler
     /// </summary>
     public static ModifierConfigHandler Instance
     {
-        get{
+        get
+        {
             if (_instance == null)
             {
                 _instance = new ModifierConfigHandler();
@@ -35,7 +37,7 @@ public class ModifierConfigHandler
     /// <param name="configFile">The Config File the handler will be responsible for.</param>
     public void InitConfig(ConfigFile configFile)
     {
-        this._configFile = configFile;
+        _configFile = configFile;
         InitConfigParams();
     }
 
@@ -46,28 +48,49 @@ public class ModifierConfigHandler
     /// </summary>
     void InitConfigParams()
     {
+        if (_configFile == null)
+        {
+            throw new InvalidOperationException(
+                "The initialization of config parameters has been called, but the"
+                    + " configuration file has not been initialized in the config handler!"
+            );
+        }
         foreach (MonsterModifierTypes modifierType in Enum.GetValues(typeof(MonsterModifierTypes)))
         {
-            ConfigEntry<int> modifierEntry = _configFile.BindConfig("Modifier Weights",
+            ConfigEntry<int> modifierEntry = _configFile.BindConfig(
+                "Modifier Weights",
                 modifierType.ToString(),
                 1,
-                "The weighting of this modifier to be applied to monsters.",
-                true);
-            configModifierWeights.Add(modifierType, modifierEntry);
-            
+                "The weighting of this modifier to be applied to monsters."
+            );
+            if (modifierEntry.Value <= 0)
+            {
+                MonsterModifiersPlugin.MonsterModifiersLogger.LogWarning(
+                    $"Weight for {modifierType.ToString()} modifier was set to less than 0 ({modifierEntry.Value}), weight has been ignored, please change this weight to an integer value that is higher than 0"
+                );
+                continue;
+            }
+            _configModifierWeights.Add(modifierType, modifierEntry);
         }
     }
 
     /// <summary>
     /// Takes in a dictionary of modifier type - modifier data key-value pairs and dynamically alters the weighting of
-    /// the modifier data, if custom config values have been chosen by the user via the mod bepinex config. 
+    /// the modifier data, if custom config values have been chosen by the user via the mod bepinex config.
     /// </summary>
     /// <param name="defaultModifiers">The default modifiers to attempt altering weighting data for.</param>
     /// <returns>The modified modifier data dictionary.</returns>
     public Dictionary<MonsterModifierTypes, ModifierData> InitCustomConfigModifiers(
-        Dictionary<MonsterModifierTypes, ModifierData> defaultModifiers)
+        Dictionary<MonsterModifierTypes, ModifierData> defaultModifiers
+    )
     {
-        foreach (var modiferEntry in configModifierWeights)
+        if (_configFile == null)
+        {
+            throw new InvalidOperationException(
+                "Config modifier weights are being queried by the Modifier Config Handler, but the initialization of the Mod's config file has not succesfully executed!"
+            );
+        }
+        foreach (var modiferEntry in _configModifierWeights)
         {
             if (!defaultModifiers.TryGetValue(modiferEntry.Key, out ModifierData defaultData))
             {
